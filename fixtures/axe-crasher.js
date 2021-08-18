ec1fa7f396f02e9e9c123853e40b5ffc9acc4219
Wilco Fiers <wilco.fiers@deque.com>
2021-08-18 16:13:34 +0200

    address comments

--- fixtures/axe-crasher.js
+++ fixtures/axe-crasher.js
@@ -3,16 +3,14 @@ if (document.documentElement.classList.contains('crash')) {
     throw new Error('Crashing axe.run(). Boom!');
   };
 
-  if (axe.utils.respondable) {
-    axe.utils.respondable.subscribe('axe.ping', () => {
-      // Makes axe-core unresponsive to other frames
-      // Timeouts will do the rest
-    });
-  }
-
   if (window.axe.runPartial) {
     window.axe.runPartial = function () {
       throw new Error('Crashing axe.runPartial(). Boom!');
     };
+  } else if (axe.utils.respondable) {
+    axe.utils.respondable.subscribe('axe.ping', () => {
+      // Makes axe-core unresponsive to other frames
+      // Timeouts will do the rest
+    });
   }
 }
