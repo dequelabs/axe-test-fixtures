e57e748d18b6ed8f018233043690f697abe6ff0a
Adam Cutler <adam.r.cutler@gmail.com>
2021-08-27 05:35:18 -0400

    fix axe-force-legacy for integrations using callback

--- fixtures/axe-force-legacy.js
+++ fixtures/axe-force-legacy.js
@@ -2,9 +2,17 @@
 delete window.axe.runPartial; 
 delete window.axe.finishRun; 
 var origRun =  window.axe.run; 
-window.axe.run = function() { 
-  return origRun.apply(window.axe, arguments).then(function (results) {
-    results.testEngine.name = 'axe-legacy'
-    return results;
-  });
+window.axe.run = async function() { 
+  if (typeof arguments[arguments.length - 1] === 'function') {
+    const oldArg = arguments[arguments.length - 1];
+    arguments[arguments.length - 1] = function(err, results) {
+      if (results) {
+        results.testEngine.name = 'axe-legacy';
+      }
+      oldArg(err, results);
+    }
+  }
+  const results = await origRun.apply(window.axe, arguments);
+  results.testEngine.name = 'axe-legacy';
+  return results;
 }
